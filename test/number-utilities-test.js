d54a021f75a866c6bf8aa4c0c0eef725bd715f2b
Dom Christie <christiedom@gmail.com>
2015-09-11 18:37:52 +0100

    Improve makeNumber edge cases

--- test/number-utilities-test.js
+++ test/number-utilities-test.js
@@ -6,10 +6,22 @@ describe('makeNumber', function () {
   it('returns the numerical value when symbols are present', function () {
     assert.equal(makeNumber('-£1,219.50'), -1219.5)
   })
+
+  it('returns null for a hyphen', function () {
+    assert.equal(makeNumber('-'), null)
+  })
+
+  it('returns null for a full stop', function () {
+    assert.equal(makeNumber('.'), null)
+  })
 })
 
 describe('makeAbsoluteNumber', function () {
   it('returns the absolute value when symbols are present', function () {
     assert.equal(makeAbsoluteNumber('-£1,219.50'), 1219.5)
+  })
+
+  it('returns null for a hyphen', function () {
+    assert.equal(makeAbsoluteNumber('-'), null)
   })
 })
