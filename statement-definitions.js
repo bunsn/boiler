b062eca38de597737abef7b42d91693723ab832c
Dom Christie <christiedom@gmail.com>
2015-09-20 15:36:05 +0100

    Add Cater Allen statement definition

--- statement-definitions.js
+++ statement-definitions.js
@@ -1,5 +1,14 @@
 var statementDefinitions = [
   {
+    institution: 'Cater Allen',
+    host: 'www.caterallenonline.co.uk',
+    columns: ['date', 'description', 'paidOut', 'paidIn', 'balance'],
+    dateFormat: 'DDMMMYYYY',
+    rows: function () {
+      return document.querySelectorAll('table[summary="Table consists of Date, Reference, Payments, Reciepts and Balance columns, displaying recent account transactions"] tbody:nth-of-type(2) tr')
+    }
+  },
+  {
     institution: 'HSBC',
     host: 'www.saas.hsbc.co.uk',
     columns: ['date', 'type', 'description', 'paidOut', 'paidIn', 'balance'],
